a1f4db08a17cbdbffd33c8144a03fcd469c09412
Sam Abdelfattah <samfatth@gmail.com>
2017-08-17 13:40:43 -0600

    answers added

---
+++ magic_eight.rb
@@ -0,0 +1,14 @@
+answers = {
+  'Yes',
+  'Of course',
+  'No way',
+  'Never',
+  'Why would you do that?',
+  'Don\'t ask me stupid questions like that.',
+  '🙄',
+  'Definitely',
+  'Duh',
+  'I guess',
+}
+
+puts '😁'
